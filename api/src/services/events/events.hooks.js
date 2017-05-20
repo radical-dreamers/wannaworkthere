6747cb94293c91f9d03e73481435aa6120657e95
Rodrigo Juliani <srodriki@gmail.com>
2017-05-20 15:57:08 -0300

    Closes #16 by adding filter logic to retrieve only published events

--- api/src/services/events/events.hooks.js
+++ api/src/services/events/events.hooks.js
@@ -2,21 +2,28 @@
 
 const { authenticate } = require('feathers-authentication').hooks;
 const searchField = require('../../hooks/common/search-field');
-const { unless, setCreatedAt, setUpdatedAt } = require('feathers-hooks-common');
+const commonHooks = require('feathers-hooks-common');
 
 module.exports = {
   before: {
     all: [
-      unless(
+      commonHooks.unless(
         (hook) => (hook.method !== 'find' || hook.method !== 'get'),
         authenticate('jwt')
       )
     ],
-    find: [searchField()],
+    find: [
+      searchField(), 
+      // if the user is not authenticated, only return those events already in Published status.
+      commonHooks.iff(
+        hook => hook.params.token, 
+        authenticate('jwt')
+      ).else ( hook => Object.assign(hook.params.query, { status: 'Published' }))
+    ],
     get: [],
-    create: [setCreatedAt('createdAt'), setUpdatedAt('updatedAt')],
-    update: [setUpdatedAt('updatedAt')],
-    patch: [setUpdatedAt('updatedAt')],
+    create: [commonHooks.setCreatedAt('createdAt'), commonHooks.setUpdatedAt('updatedAt')],
+    update: [commonHooks.setUpdatedAt('updatedAt')],
+    patch: [commonHooks.setUpdatedAt('updatedAt')],
     remove: []
   },
 
